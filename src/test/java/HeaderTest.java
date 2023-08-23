dd1f9cbb763a441fd8975a510198bb0708e480bc
dbahmatskiy <bahdiman@gmail.com>
2023-08-23 22:50:49 +0300

    HeaderTest

---
+++ src/test/java/HeaderTest.java
@@ -0,0 +1,16 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HeaderTest {
+
+    @Test
+    public void testHeader() {
+        Response response = RestAssured.get("https://playground.learnqa.ru/api/homework_header")
+                .andReturn();
+        String header = response.getHeader("x-secret-homework-header");
+        assertEquals("Some secret value", header, "Header isn`t correct");
+    }
+}
